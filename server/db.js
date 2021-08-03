e38bd57b7d9f031d30e1e3302c086c76e9efa36a
Christoffer Lambertsson <github@lambertsson.me>
2021-08-03 23:02:06 +0200

    Fix more birds and some case sensitivity

--- server/db.js
+++ server/db.js
@@ -1,28 +1,29 @@
-const {Datastore} = require('@google-cloud/datastore');
-const uuid = require("uuid")
+const { Datastore } = require("@google-cloud/datastore");
+const uuid = require("uuid");
 
 // Creates a client
 const datastore = new Datastore();
 
-const KvitterKind = "Kvitter"
+const KvitterKind = "Kvitter";
 
 async function getKvitter() {
-  const query = datastore.createQuery(KvitterKind).order("created_at");
+  const query = datastore.createQuery(KvitterKind).order("created_at", {
+    descending: true,
+  });
   const [kvitter, msg] = await datastore.runQuery(query);
-  console.debug(msg)
-  return kvitter
+  console.debug(msg);
+  return kvitter;
 }
 
-const storeKvitter = async kvitter => {
+const storeKvitter = async (kvitter) => {
   const key = datastore.key([KvitterKind, uuid.v4()]);
   const [rsp] = await datastore.save({
     key: key,
-    data: kvitter
-  })
-  console.debug(rsp)
-}
+    data: kvitter,
+  });
+};
 
 module.exports = {
   getKvitter,
   storeKvitter,
-}
+};
--- server/library/Kvitterfy.js
+++ server/library/Kvitterfy.js
@@ -1,12 +1,28 @@
-const kvitters = ["kvitt", "tuut", "krax", "piip", "kvidder", "bripp"];
+const { BIRDS } = require("./Birds");
 
-const birds = ["Blåmesen", "Måsen", "Kråkan"];
+const kvitters = ["kvitt", "tuut", "krax", "piip", "kvidder", "bripp", "tweet", "purr"];
 
 const getWordHash = (word) => word.split("").reduce((sum, c) => sum + c.charCodeAt(0), 0);
 
+const isCapitalized = (word) =>
+  (word[0] >= "A" && word[0] <= "Z") || word[0] === "Å" || word[0] === "Ä" || word[0] === "Ö";
+
+const isUppercase = (word) => {
+  const letters = word.split("");
+  return letters.every(isCapitalized);
+};
+
+const replaceWord = (word) => {
+  const kvitter = kvitters[getWordHash(word) % kvitters.length];
+  const [firstLetter, ...rest] = kvitter;
+  if (isUppercase(word)) return kvitter.toUpperCase();
+  if (isCapitalized(word)) return `${firstLetter.toUpperCase()}${rest.join("")}`;
+  return kvitter;
+};
+
 const kvitterfy = (kvitt) => {
-  const message = kvitt.message.replace(/[A-Za-z]\w*/g, (word) => kvitters[getWordHash(word) % kvitters.length]);
-  const user = birds[getWordHash(kvitt.user) % birds.length];
+  const message = kvitt.message.replace(/[\w|Å|Ä|Ö|å|ä|ö]+/g, replaceWord);
+  const user = BIRDS[getWordHash(kvitt.user) % BIRDS.length];
 
   return {
     ...kvitt,
