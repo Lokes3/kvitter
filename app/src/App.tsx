fa3d965a44e49552cea3e39bc16e9abf8b249468
Adrian Rågberg <adrian.ragberg@tritech.se>
2021-08-03 12:45:29 +0200

    Added basic html

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="/">
+        <label>Kvitt: </label>
+        <input type="text" id="kvitt" name="kvitt"/>
+      </form>
+
+      <div className="feed">
+        Kvitt kvitt kvitt kvivitt kvitt kvitt! D: - Sparv<br/>
+        Kvitt kvikvitt kvivivitt, kvitt kvitt ^^ - Blåmes<br/>
+        Kvitt 🐥- Höna<br/>
+        KRAAAAAAA! - Skata
+      </div>
     </div>
   );
 }
