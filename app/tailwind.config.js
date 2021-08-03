dba66a061c02ff47fe031903452e64cf2b541d57
Christoffer Lambertsson <github@lambertsson.me>
2021-08-04 00:06:39 +0200

    Try again

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    safelist: [
+      "bg-gradient-to-tr",
+      "bg-gradient-to-tl",
+      "bg-gradient-to-br",
+      "bg-gradient-to-bl",
+      "from-blue-500",
+      "from-red-500",
+      "from-yellow-500",
+      "from-purple-500",
+      "from-pink-500",
+      "to-blue-500",
+      "to-red-500",
+      "to-yellow-500",
+      "to-purple-500",
+      "to-pink-500",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,20 +26,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-  safelist: [
-    "bg-gradient-to-tr",
-    "bg-gradient-to-tl",
-    "bg-gradient-to-br",
-    "bg-gradient-to-bl",
-    "from-blue-500",
-    "from-red-500",
-    "from-yellow-500",
-    "from-purple-500",
-    "from-pink-500",
-    "to-blue-500",
-    "to-red-500",
-    "to-yellow-500",
-    "to-purple-500",
-    "to-pink-500",
-  ],
 };
